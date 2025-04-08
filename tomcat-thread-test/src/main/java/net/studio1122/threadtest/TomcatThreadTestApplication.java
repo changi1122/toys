06e2984e0732f97703e6b17b9c2871cec573c59d
Woochang Lee <changi112242@gmail.com>
2025-04-08 16:12:43 +0900

    Embbedd Tomcat Thread&Queue 테스트

---
+++ tomcat-thread-test/src/main/java/net/studio1122/threadtest/TomcatThreadTestApplication.java
@@ -0,0 +1,13 @@
+package net.studio1122.threadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatThreadTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatThreadTestApplication.class, args);
+    }
+
+}
---
+++ tomcat-thread-test/src/test/java/net/studio1122/threadtest/TomcatThreadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.studio1122.threadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatThreadTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
