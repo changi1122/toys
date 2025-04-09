0dcca1f375994099838a77155e8a06cb458d4e66
Woochang Lee <changi112242@gmail.com>
2025-04-09 17:48:33 +0900

    Embbedd Tomcat Thread&Queue 테스트 완료

--- tomcat-thread-test/src/main/java/net/studio1122/threadtest/TestController.java
+++ tomcat-thread-test/src/main/java/net/studio1122/threadtest/TestController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
     @GetMapping("/test")
     public String test() throws InterruptedException {
         Thread.sleep(10000); // 10초 대기
         return "Done";
     }
-
 }
