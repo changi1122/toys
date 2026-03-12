96bc20b02acba6dcc5c0d0c4e5e9ed4947611329
Woochang Lee <changi112242@gmail.com>
2026-03-12 21:58:13 +0900

    React Typescript 연습 프로젝트 etc

---
+++ ts-react-shopping-cart/src/components/game/Square.tsx
@@ -0,0 +1,26 @@
+type PropsType = {
+    value: string,
+    onSquareClick?: () => void,
+}
+
+export default function Square({ value, onSquareClick }: PropsType) {
+  return (
+    <button
+      style={{
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 0,
+        backgroundColor: '#fff',
+        border: '1px solid #999',
+        outline: 0,
+        borderRadius: 0,
+        fontSize: '1rem',
+        fontWeight: 'bold',
+      }}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
